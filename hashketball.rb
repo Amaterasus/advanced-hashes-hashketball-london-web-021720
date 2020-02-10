760ec4a19c8ef4bca6957605f16c866b9e3125dc
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 17:57:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -268,11 +268,16 @@ def player_with_longest_name
 end
 
 def long_name_steals_a_ton?
-  longest_name = player_with_longest_name
+  longest_name = player_with_longest_name(game_hash)
   longest_name_steals = 0
   other_most_steals = 0
   
   game_hash.each do |status, team|
-    
+    team[:players].each do |team_player|
+      if team_player[:player_name] == longest_name
+        longest_name_steals = team_player[:steals]
+      else
+        other_most_steals = team_player[:steals]
+      end
   end
 end
