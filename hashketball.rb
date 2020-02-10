c696911e75be433d7a01ed95c5f954c5abda5f27
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 17:55:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -268,5 +268,11 @@ def player_with_longest_name
 end
 
 def long_name_steals_a_ton?
-  true
+  longest_name = player_with_longest_name
+  longest_name_steals = 0
+  other_most_steals = 0
+  
+  game_hash.each do |status, team|
+    
+  end
 end
